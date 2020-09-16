9ba95868cb4b6eb453f23fb55e3784fa76676eae
Stefano Bettinelli <ste.bettinelli@gmail.com>
2020-09-16 12:43:09 +0200

    manage.py update to handle custom directory for migrations files

--- api/manage.py
+++ api/manage.py
@@ -1,10 +1,14 @@
-from flask_script import Manager
+import os
+
 from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 from flaskr import app
 from models import db
 
-migrate = Migrate(app, db)
+MIGRATION_DIR = os.path.join("api", "migrations")
+
+migrate = Migrate(app, db, directory=MIGRATION_DIR)
 manager = Manager(app)
 
 manager.add_command("db", MigrateCommand)
