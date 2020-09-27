02b3565649212ca94dba3dc51932994d3527bdd7
Stefano Bettinelli <ste.bettinelli@gmail.com>
2020-09-27 16:08:08 +0200

    Styling fixes for rankings

--- src/components/Rankings/RankItem.jsx
+++ src/components/Rankings/RankItem.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { ListItem, ListItemText } from "@material-ui/core";
 import PropTypes from "prop-types";
+import useStyle from "./styles";
 
 function RankItem({ data, index, style }) {
+  const classes = useStyle();
   if (!data) return null;
 
   const item = data[index];
 
   return (
     <ListItem button style={style} key={index}>
-      <ListItemText primary={item.score} />
+      <ListItemText
+        primary={index + 1}
+        classes={{ root: classes.positionRoot }}
+      />
       <ListItemText primary={item.nickname} />
+      <ListItemText
+        primary={item.score}
+        classes={{ root: classes.scoreRoot }}
+      />
     </ListItem>
   );
 }
---
+++ src/components/Rankings/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles({
+  positionRoot: {
+    maxWidth: "24px",
+  },
+  scoreRoot: {
+    textAlign: "right",
+  },
+});
